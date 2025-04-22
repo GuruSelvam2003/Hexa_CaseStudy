207167fc60879d1b06262cf1007f97ac4931ecfc
GuruSelvam2003 <159774358+GuruSelvam2003@users.noreply.github.com>
2025-04-22 10:07:02 +0530

    Add files via upload

---
+++ c#/Entity/Expense.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financial_Management
+{
+    public class Expense
+    {
+        public int ExpenseId { get; set; }
+        public int UserId { get; set; }
+        public decimal Amount { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+
+        public Expense() { }
+
+        public Expense(int userId, decimal amount, int categoryId, DateTime date, string description)
+        {
+            UserId = userId;
+            Amount = amount;
+            CategoryId = categoryId;
+            Date = date;
+            Description = description;
+        }
+    }
+}
---
+++ c#/Entity/ExpenseCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financial_Management
+{
+    public class ExpenseCategory
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+
+        public ExpenseCategory(string categoryName)
+        {
+            CategoryName = categoryName;
+        }
+    }
+}
