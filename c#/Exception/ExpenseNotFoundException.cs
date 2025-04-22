81b9132b30a4571b1aaf89f9506a155b3df02004
GuruSelvam2003 <159774358+GuruSelvam2003@users.noreply.github.com>
2025-04-22 10:09:19 +0530

    Add files via upload

---
+++ c#/Exception/ExpenseNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myexceptions
+{
+    public class ExpenseNotFoundException : Exception
+    {
+        public ExpenseNotFoundException() : base("Expense ID not found in the database.") { }
+
+        public ExpenseNotFoundException(string message) : base(message) { }
+    }
+}
---
+++ c#/Exception/UserNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myexceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException() : base("User ID not found in the database.") { }
+
+        public UserNotFoundException(string message) : base(message) { }
+
+    }
+}
