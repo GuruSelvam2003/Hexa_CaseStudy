5437f595a25357c8e0daa36d3d64cbffcc985d4b
GuruSelvam2003 <159774358+GuruSelvam2003@users.noreply.github.com>
2025-04-22 10:11:05 +0530

    Add files via upload

---
+++ c#/Util/DBConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace Financial_Management
+{
+    public class DBConnection
+    {
+        private static SqlConnection connection;
+
+        public static SqlConnection GetConnection()
+        {
+            try
+            {
+                string path = "D:\\Hexaware\\dbpath\\db_props.txt";
+                string connectionString = PropertyUtil.GetConnectionString(path);
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Database connection failed: " + ex.Message);
+            }
+
+            return connection;
+        }
+    }
+}
---
+++ c#/Util/PropertyUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financial_Management
+{
+    public class PropertyUtil
+    {
+        public static string GetConnectionString(string filePath)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+
+
+                string server = lines[0].Trim();
+                string database = lines[1].Trim();
+
+                return $"Server={server};Database={database};Integrated Security=True;TrustServerCertificate=True;";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading property file: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
